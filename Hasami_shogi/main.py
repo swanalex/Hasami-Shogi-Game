d21228f04950ba42203aa84e974b118e9f463a37
swanalex <swanalex@oregonstate.edu>
2023-01-12 18:09:13 -0500

    Add congratulation message when player wins

--- Hasami_shogi/main.py
+++ Hasami_shogi/main.py
@@ -20,9 +20,16 @@ def main():
     clock = pygame.time.Clock()
     board = HasamiShogiGame()
 
+## practice blitting things to the screen
+
+    pygame.font.init()
+    font_1 = pygame.font.SysFont('freesansbold.ttf', 55)
+
+    black_win = font_1.render('Congratulations, Black has won!', True, (0,0,0))
+    red_win = font_1.render('Congratulations, Red has won!', True, (0,0,0))
+
 
     while run:
-        black_victory = 'CONGRATULATIONS, black has won!'
         clock.tick(FPS)
         
         for event in pygame.event.get():
@@ -36,6 +43,7 @@ def main():
                 c = str(col)
                 rc = r+c
                 #piece = board.get_square_occupant(rc)
+
             if event.type == pygame.MOUSEBUTTONUP:
                 pos2 = pygame.mouse.get_pos()
                 row2, col2 = get_row_col_from_mouse(pos2)
@@ -46,6 +54,17 @@ def main():
                 
 
         board.draw(WIN)
+        
+        # Prints congratulations message to the screen
+        if board.get_game_state() == 'BLACK_WON':
+            textRect = black_win.get_rect()
+            textRect.center = (WIDTH//2, HEIGHT//2)
+            WIN.blit(black_win, textRect)
+        elif board.get_game_state() == 'RED_WON':
+            textRect = red_win.get_rect()
+            textRect.center = (WIDTH//2, HEIGHT//2)
+            WIN.blit(red_win, textRect)
+
         pygame.display.update()
         
 
